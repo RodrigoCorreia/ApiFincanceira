cd8118a7bb144aa568754a2f2a328bbaa37fd1c3
Rodrigo Correia <rodrigocorrei@gmail.com>
2022-01-11 11:28:11 -0300

    test

--- src/routes/apiStatusRoute.test.ts
+++ src/routes/apiStatusRoute.test.ts
@@ -7,7 +7,6 @@ describe('ApiStatusRoute', () => {
 	it('Should return message that server is on', async () => {
 		const response = await request.get('/');
 		expect(response.status).toBe(200);
-		expect(response.status).toBe(200);
 		expect(response.body.message).toBe('Server is running...');
 	});
 });
